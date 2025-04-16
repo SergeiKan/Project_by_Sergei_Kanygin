187f27b5e41d230836a6b1c6d3454522572f5326
SergeiKan <kanygin.s.a@gmail.com>
2025-04-16 21:43:31 +0200

    Function to delete a book from the library

--- main.py
+++ main.py
@@ -5,9 +5,8 @@ library = []
 def add_book():
     title = input("Enter book title: ")
     author = input("Enter an author: ")
-
 # Add the book to the list
-    library.append({"title": title, "author": author})
+    library.append({"title": title, "author": author, "user": None})
     print("The book was successfully added!")
 
 # Function to display all books in the library
@@ -17,6 +16,18 @@ def view_books():
     else:
         print("List of books: ")
         for index, book in enumerate(library, start=1):
-            # Check if book is borrowed by user and add information
-            user_info = f" (borrowed by: {book['user']})" if book['user'] else " (available)"
-            print(f"{index}. {book['title']} - {book['author']}{user_info}")
+            print(f"{index}. {book['title']} - {book['author']}")
+
+# Function to delete a book from the library
+def delete_book():
+    # Display the list with books
+    view_books()
+    try:
+        book_number = int(input("Enter the book number to delete: "))
+        if 1 <= book_number <= len(library):
+            removed_book = library.pop(book_number - 1)
+            print(f"The book '{removed_book['title']}' has been deleted.")
+        else:
+            print("Invalid book number.")
+    except ValueError:
+        print("Please enter a valid number.")

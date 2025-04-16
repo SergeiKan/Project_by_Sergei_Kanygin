556b9ca596e2e0bf0b762aba0c6e8cf74126c6f0
SergeiKan <kanygin.s.a@gmail.com>
2025-04-16 14:22:10 +0200

    First step

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print('Hello')
-print('World')
+# Create list for book`s information in library
+library = []
+
+# Add a new book in library
+def add_book():
+    title = input("Enter book title: ")
+    author = input("Enter an author: ")
+# Add the book to the list
+    library.append({"title": title, "author": author})
+    print("The book was successfully added!")
+
+add_book()
+print(f"Library: {library}")

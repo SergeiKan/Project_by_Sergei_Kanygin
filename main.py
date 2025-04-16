e0d533e1055b8264dab3688013c6cf7b4a50e6ae
SergeiKan <kanygin.s.a@gmail.com>
2025-04-16 21:34:28 +0200

    Function to display all books in the library

--- main.py
+++ main.py
@@ -5,9 +5,18 @@ library = []
 def add_book():
     title = input("Enter book title: ")
     author = input("Enter an author: ")
+
 # Add the book to the list
     library.append({"title": title, "author": author})
     print("The book was successfully added!")
 
-add_book()
-print(f"Library: {library}")
+# Function to display all books in the library
+def view_books():
+    if not library:
+        print("The library is empty.")
+    else:
+        print("List of books: ")
+        for index, book in enumerate(library, start=1):
+            # Check if book is borrowed by user and add information
+            user_info = f" (borrowed by: {book['user']})" if book['user'] else " (available)"
+            print(f"{index}. {book['title']} - {book['author']}{user_info}")
